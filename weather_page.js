e9deaf3c9837e7a8166a05d9360d7186a256a44c
nguyenblacks <nguyenblacksa@gmail.com>
2026-05-16 13:12:55 +0200

    feat: add browser navigation controls and update default weather source to yr.no

--- weather_page.js
+++ weather_page.js
@@ -21,9 +21,24 @@ class WeatherPage extends Gtk.Box {
         const titleWidget = new Adw.WindowTitle({ title: "Weather Platform", subtitle: "Live Satellite & Forecast" });
         header.set_title_widget(titleWidget);
 
-        const refreshBtn = new Gtk.Button({ icon_name: 'view-refresh-symbolic' });
+        // WebView Navigation Controls
+        const webBackBtn = new Gtk.Button({ icon_name: 'go-previous-symbolic', tooltip_text: 'Back' });
+        webBackBtn.connect('clicked', () => this.webView.go_back());
+        
+        const webForwardBtn = new Gtk.Button({ icon_name: 'go-next-symbolic', tooltip_text: 'Forward' });
+        webForwardBtn.connect('clicked', () => this.webView.go_forward());
+
+        header.pack_start(webBackBtn);
+        header.pack_start(webForwardBtn);
+
+        const refreshBtn = new Gtk.Button({ icon_name: 'view-refresh-symbolic', tooltip_text: 'Reload' });
         refreshBtn.connect('clicked', () => this.webView.reload());
+        
+        const homeBtn = new Gtk.Button({ icon_name: 'go-home-symbolic', tooltip_text: 'Home' });
+        homeBtn.connect('clicked', () => this.webView.load_uri('https://www.yr.no'));
+        
         header.pack_end(refreshBtn);
+        header.pack_end(homeBtn);
 
         this.append(header);
 
@@ -33,8 +48,8 @@ class WeatherPage extends Gtk.Box {
             hexpand: true
         });
 
-        // Use weather.com for full weekly forecasts
-        this.webView.load_uri('https://weather.com');
+        // Use yr.no for highly accurate Scandinavian meteorology data
+        this.webView.load_uri('https://www.yr.no');
         
         this.append(this.webView);
     }
